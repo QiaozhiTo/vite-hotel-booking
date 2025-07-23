9b79a4ad5493cc467ceeda3ba73d8dccdd0971f1
Qiaozhi Bao <qiaozhibao96@gmail.com>
2025-07-22 23:11:07 -0400

    NAVBAR + homepage setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 // type rafce to create the component structure
 import React from 'react'
 import Navbar from './components/Navbar'
-import { useLocation } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import Home from './pages/Home';
 const App = () => {
 
   const isOwnerpath = useLocation().pathname.includes("owner"); /*true*/
@@ -11,6 +12,13 @@ const App = () => {
     <div>
       {/* not owner dashboard to show Navbar, else not showing up Navbar */}
       {!isOwnerpath && <Navbar />}
+      <div className='min-h-[70vh'>
+        <Routes>
+          <Route path='/' element = {<Home/>}></Route>
+
+        </Routes>
+
+      </div>
     </div>
   )
 }

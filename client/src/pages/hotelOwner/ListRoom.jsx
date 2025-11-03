08ced8620111e82824f1db9cf5ed15bdff157cd6
Qiaozhi Bao <qiaozhibao96@gmail.com>
2025-11-02 22:02:05 -0500

    addroom

--- client/src/pages/hotelOwner/ListRoom.jsx
+++ client/src/pages/hotelOwner/ListRoom.jsx
@@ -4,9 +4,7 @@ import Title from '../../components/Title'
 const ListRoom = () => {
   return (
     <div>
-        <Title align='left' font='outfit' title="Listing Room" subTitle = 'View, edit, or manage all listed rooms. Keep the information up-to-date to provide the best experience for users.
-
-'></Title>
+        <Title align='left' font='outfit' title="Listing Room" subTitle = 'View, edit, or manage all listed rooms. Keep the information up-to-date to provide the best experience for users.'></Title>
     </div>
   )
 }
